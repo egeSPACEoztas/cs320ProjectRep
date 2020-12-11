ef357b6b69c2e4e637155ebd334df610612b7c79
burakaltintas011801 <72281243+burakaltintas011801@users.noreply.github.com>
2020-12-11 15:15:35 +0300

    Create Product.java
    Product class and constructor has been implemented.

---
+++ Product.java
@@ -0,0 +1,13 @@
+
+
+        public class Product {
+            int id;
+            int quantity;
+
+            Product(int id, int quantity) {
+                this.id = id;
+                this.quantity = quantity;
+            }
+        }
+
+
