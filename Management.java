a9e8cf9f08925aff3598d0f7a5c2a24b3ccf4da2
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2020-12-13 19:10:31 +0300

    Update Management.java
    Database class name has been corrected and the password is created for managers

--- Management.java
+++ Management.java
@@ -1,19 +1,20 @@
 public class Management {
 
+    public static final int password = 1235;
 
     public static void addProduct(int productID, int quantity){
-        DataBase.DBincrementQuantity(productID,quantity);
+        Database.DBincrementQuantity(productID,quantity);
     }
 
     public static void removeProduct(int productID){
-        DataBase.removeFromDB(productID);
+        Database.removeFromDB(productID);
     }
 
     public static void addNewProduct(int productID , int quantity, String brand, double price, String name){
-        DataBase.DBaddProduct(productID, quantity, brand, price, name);
+        Database.DBaddProduct(productID, quantity, brand, price, name);
     }
 
     public static void updatePrice(int productID, double newPrice){
-        DataBase.DBupdatePrice(productID,newPrice);
+        Database.DBupdatePrice(productID,newPrice);
     }
 }
