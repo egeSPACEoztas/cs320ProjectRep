1c7fded82af70fbdfa603010275e68675619019d
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2020-12-14 20:56:06 +0300

    GUI class has been added
    Main screen is added

---
+++ GUI/formMain.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class formMain extends JFrame {
+
+
+    private JButton button_manager;
+    private JButton button_customer;
+    private JPanel panel_main;
+
+    public formMain(){
+        add(panel_main);
+        setSize(600,600);
+        setTitle("MMS");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        button_manager.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+
+                formPassword password = new formPassword();
+                password.setVisible(true);
+            }
+        });
+
+        button_customer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //customer framei çağıralacak
+            }
+        });
+
+    }
+}
---
+++ GUI/formPassword.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class formPassword extends JFrame {
+    private JPanel panel_password;
+    private JPasswordField passwordField1;
+    private JButton button_password;
+    private JLabel label_password;
+
+    public formPassword(){
+        add(panel_password);
+        setSize(600,600);
+        setTitle("MMS");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        button_password.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String password_manager, password_field;
+
+                password_manager = String.valueOf(Management.password);
+                password_field = new String(passwordField1.getPassword());
+
+                if(password_field.equals(password_manager)){
+                    //maganer formu çağıracak
+                    System.out.println("Selamun aleyküm");
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "Invalid Password \nTry Again");
+                    passwordField1.setText("");
+                }
+            }
+        });
+
+    }
+}
