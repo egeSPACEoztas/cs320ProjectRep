c6f855f5a92b8a00855dcf39a0633be56249294e
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2020-12-16 00:49:47 +0300

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,13 +1,10 @@
+public class Product {
+    public int id;
+    public int quantity;
 
+    public Product(int id, int quantity) {
+        this.id = id;
+        this.quantity = quantity;
+    }
 
-        public class Product {
-            int id;
-            int quantity;
-
-            Product(int id, int quantity) {
-                this.id = id;
-                this.quantity = quantity;
-            }
-        }
-
-
+}
