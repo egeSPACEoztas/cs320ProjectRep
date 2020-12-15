c15caaebcc48615b199dcce2c6f34c36311e1892
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2020-12-16 01:02:30 +0300

    getProduct method is updated ,some extra "\" symbols are deleted and getPrice method implemented

--- Database Files/database.java
+++ Database Files/database.java
@@ -1,166 +1,194 @@
 
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Database {
-	private static final String URL = "jdbc:mysql://localhost:3306/cs320?useSSL=false&useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey";
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "XaX404d@nk=";
+    private static final String URL = "jdbc:mysql://localhost:3306/cs320?useSSL=false&useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "XaX404d@nk=";
 
-	private static Connection connection = null;
+    private static Connection connection = null;
 
-	public static void establishConnection() {
-		try {
-			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+    public static ArrayList<items> products = new ArrayList<items>();
 
-	public static void closeConnection() {
-		try {
-			if (connection != null) {
-				connection.close();
-			}
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-	}
+    public static void establishConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-//******************************************************************		    
-	// LIST PRODUCST IN STOCKS
-	public static void getProducts() {
-		try {
-			establishConnection();
-			Statement statement = connection.createStatement();
+    public static void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-			String selectQuery = "SELECT * FROM STOCKS NATURAL JOIN PRODUCT WHERE squantity > 0 \"";
-			ResultSet rs = statement.executeQuery(selectQuery);
+    //******************************************************************
+    // LIST PRODUCST IN STOCKS
+    public static void getProducts() {
+        try {
+            establishConnection();
+            Statement statement = connection.createStatement();
+            String cols = "";
+            String selectQuery = "SELECT * FROM STOCKS NATURAL JOIN PRODUCT WHERE squantity > 0";
+            ResultSet rs = statement.executeQuery(selectQuery);
 
-			while (rs.next()) {
-				System.out.println("pid: " + rs.getString("pid") + " | Product's Name: " + rs.getString("pname")
-						+ " | Product's Price: " + rs.getString("pprice") + " | Product's Brand: "
-						+ rs.getString("pbrand") + " | Quantity: " + rs.getString("squantity"));
-			}
-			rs.close();
-			statement.close();
-			closeConnection();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-	}
 
-//****************************************************
-	// REMOVE PRODUCTS from STOCKS
-	public static void removeFromDB(int pid) {
-		try {
-			establishConnection();
-			Statement statement = connection.createStatement();
+            items items;
+            while (rs.next()) {
+                items = new items (rs.getInt("pid"), rs.getString("pname") , rs.getDouble("pprice") ,rs.getString("pbrand") , rs.getInt("squantity"));
+                products.add(items);
+            }
 
-			String delete_stocks = "SELECT * FROM STOCKS";
-			ResultSet rs = statement.executeQuery(delete_stocks);
+            rs.close();
+            statement.close();
+            closeConnection();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-			if (rs.next()) {
-				String deleteQuery = ("DELETE FROM STOCKS WHERE pid=\"" + pid + "\"");
-				statement.executeUpdate(deleteQuery);
-				System.out.println("Process completed!");
-			} else {
-				System.out.println("WARNING! In stocks, there is no product with the given product id.");
-			}
-			rs.close();
-			statement.close();
-			closeConnection();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-	}
+    //****************************************************
+    // REMOVE PRODUCTS from STOCKS
+    public static void removeFromDB(int pid) {
+        try {
+            establishConnection();
+            Statement statement = connection.createStatement();
 
-//************************************************************
-	// Decreasing Quantity
-	public static void DBdecrementQuantity(int pid, int squantity) {
-		try {
-			establishConnection();
-			Statement statement = connection.createStatement();
+            String delete_stocks = "SELECT * FROM STOCKS";
+            ResultSet rs = statement.executeQuery(delete_stocks);
 
-			String selectQuery = "(SELECT * FROM STOCKS WHERE pid=\"" + pid + "\")";
-			ResultSet rs = statement.executeQuery(selectQuery);
+            if (rs.next()) {
+                String deleteQuery = ("DELETE FROM STOCKS WHERE pid=\"" + pid + "\"");
+                statement.executeUpdate(deleteQuery);
+                System.out.println("Process completed!");
+            } else {
+                System.out.println("WARNING! In stocks, there is no product with the given product id.");
+            }
+            rs.close();
+            statement.close();
+            closeConnection();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-			if (rs.getInt("squantity") >= squantity) {
-				String updateQuery = ("UPDATE STOCKS SET squantity = squantity -\"" + squantity + "\" WHERE pid=\""
-						+ pid + "\"");
-				statement.executeUpdate(updateQuery);
-				System.out.println("\nProcess completed!");
+    //************************************************************
+    // Decreasing Quantity
+    public static void DBdecrementQuantity(int pid, int squantity) {
+        try {
+            establishConnection();
+            Statement statement = connection.createStatement();
 
-			} else {
-				System.out.println("There are no enough products in stock.");
-			}
+            String selectQuery = "SELECT * FROM STOCKS WHERE pid=" + pid;
+            ResultSet rs = statement.executeQuery(selectQuery);
 
-			rs.close();
-			statement.close();
-			closeConnection();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+            rs.next();
+            if (rs.getInt("squantity") >= squantity) {
+                String updateQuery = "UPDATE STOCKS SET squantity = squantity -" + squantity + " WHERE pid=" + pid ;
+                statement.executeUpdate(updateQuery);
+                System.out.println("\nProcess completed!");
 
-//*******************************************************
-	// increase QUANTITY
-	public static void DBincrementQuantity(int pid, double squantity) {
-		try {
-			establishConnection();
-			Statement statement = connection.createStatement();
+            } else {
+                System.out.println("There are no enough products in stock.");
+            }
 
-			String updateQuery = "UPDATE STOCKS SET squantity= squantity + \"" + squantity + "\" WHERE pid= \"" + pid
-					+ "\")";
-			statement.executeUpdate(updateQuery);
-			System.out.println("\nProcess completed!");
+            rs.close();
+            statement.close();
+            closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-			statement.close();
-			closeConnection();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-	}
+    //*******************************************************
+    // increase QUANTITY
+    public static void DBincrementQuantity(int pid, double squantity) {
+        try {
+            establishConnection();
+            Statement statement = connection.createStatement();
 
-//***************************************
-	// NEW PRODUCT with QUANTITY
-	public static void DBaddProduct(int pid, int squantity, String pbrand, double pprice, String pname) {
-		try {
-			establishConnection();
-			Statement statement = connection.createStatement();
+            String updateQuery = "UPDATE STOCKS SET squantity= squantity + \"" + squantity + "\" WHERE pid= \"" + pid
+                    + "\")";
+            statement.executeUpdate(updateQuery);
+            System.out.println("\nProcess completed!");
 
-			String insertProduct = "INSERT INTO PRODUCT (pid, pbrand, pprice, pname) VALUES (\"" + pid + "\" ,\""
-					+ pbrand + "\" ,\"" + pprice + "\", \"" + pname + "\")";
-			statement.executeUpdate(insertProduct);
+            statement.close();
+            closeConnection();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-			String insertQuantity = "INSERT INTO STOCKS (pid, squantity) VALUES (\"" + pid + "\", \"" + squantity
-					+ "\")";
-			statement.executeUpdate(insertQuantity);
+    //***************************************
+    // NEW PRODUCT with QUANTITY
+    public static void DBaddProduct(int pid, int squantity, String pbrand, double pprice, String pname) {
+        try {
+            establishConnection();
+            Statement statement = connection.createStatement();
 
-			System.out.println("\nProcess completed!");
-			statement.close();
-			closeConnection();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-	}
+            String insertProduct = "INSERT INTO PRODUCT (pid, pbrand, pprice, pname) VALUES (\"" + pid + "\" ,\""
+                    + pbrand + "\" ,\"" + pprice + "\", \"" + pname + "\")";
+            statement.executeUpdate(insertProduct);
 
-// *******************************************************
-	// UPDATE PRICE
-	public static void DBupdatePrice(int pid, double pprice) {
-		try {
-			establishConnection();
-			Statement statement = connection.createStatement();
+            String insertQuantity = "INSERT INTO STOCKS (pid, squantity) VALUES (\"" + pid + "\", \"" + squantity
+                    + "\")";
+            statement.executeUpdate(insertQuantity);
 
-			String updateQuery = "UPDATE PRODUCT SET pprice= \"" + pprice + "\" WHERE pid= \"" + pid + "\")";
-			statement.executeUpdate(updateQuery);
-			System.out.println("\nProcess completed!");
+            System.out.println("\nProcess completed!");
+            statement.close();
+            closeConnection();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-			statement.close();
-			closeConnection();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-	}
+    // *******************************************************
+    // UPDATE PRICE
+    public static void DBupdatePrice(int pid, double pprice) {
+        try {
+            establishConnection();
+            Statement statement = connection.createStatement();
+
+            String updateQuery = "UPDATE PRODUCT SET pprice= \"" + pprice + "\" WHERE pid= \"" + pid + "\")";
+            statement.executeUpdate(updateQuery);
+            System.out.println("\nProcess completed!");
+
+            statement.close();
+            closeConnection();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static double getPrice(int id){
+        double price = 0.0;
+        try {
+
+            establishConnection();
+            Statement statement = connection.createStatement();
+            String selectQuery = "select pprice from product where pid=" +id;
+            ResultSet rs = statement.executeQuery(selectQuery);
+
+            if(rs.next()){
+                price += rs.getDouble("pprice");
+            }
+
+            rs.close();
+            statement.close();
+            closeConnection();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return price;
+    }
 }
