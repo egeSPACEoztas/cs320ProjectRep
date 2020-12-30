537f18f5d1f18014dc21162c152836b3dcfbce08
ardasumbul1 <38762229+ardasumbul1@users.noreply.github.com>
2020-12-30 11:15:33 +0300

    database sample is added.

--- Database Files/DML.sql
+++ Database Files/DML.sql
@@ -1,8 +1,46 @@
 INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 1, 'NEO', 420.69 , 'CASPER');
-INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 2, 'PS5', 9999.99 , 'SONY');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 2, 'PS5', 500.99 , 'SONY');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 3, 'PS4', 420.69 , 'SONY');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 4, 'TUNE550BT', 20.99 , 'JBL');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 5, 'IPHONE7', 400.69 , 'APPLE');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 6, 'MATEBOOKX', 2000 , 'HUAWEI');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 7, 'C40 INISSIA', 80.69 , 'NESPRESSO');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 8, 'GAMEPOWER 27', 399.99 , 'ASUS');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 9, 'VIGOR GK20', 120.69 , 'MSI');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 10, '512SSD',199.99 , 'TOSHIBA');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 11, 'IPHONE8', 320.69 , 'APPLE');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 12, 'INSTAX NEO', 89.99 , 'FUJIFILM');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 13, 'MINI FIRIN', 120.69 , 'ARÇELİK');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 14, '32''TV',219.99 , 'LG');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 15, 'GTA V', 120.69 , 'ROCKSTAR');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 16, 'FIFA20', 99.99 , 'EA SPORTS');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 17, 'IPAD PRO 2020', 520.69 , 'APPLE');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 18, 'TOST MAKİNESİ',100.79 , 'VESTEL');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 19, 'MACBOOKPRO 2019', 1020.19 , 'APPLE');
+INSERT INTO PRODUCT (pid, pname, pprice, pbrand) VALUES ( 20, 'FREEBUDS 3İ', 112.99 , 'HUAWEI');
 
 
 
 
-INSERT INTO STOCKS (pid,squantity) VALUES ( 1,10);
-INSERT INTO STOCKS (pid,squantity) VALUES ( 2,10);
+
+INSERT INTO STOCKS (pid,squantity) VALUES ( 1,105);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 2,102);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 3,232);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 4,1120);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 5,1043);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 6,110);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 7,170);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 8,1120);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 9,912);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 10,550);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 11,180);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 12,643);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 13,874);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 14,125);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 15,249);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 16,115);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 17,132);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 18,235);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 19,733);
+INSERT INTO STOCKS (pid,squantity) VALUES ( 20,634);
+
